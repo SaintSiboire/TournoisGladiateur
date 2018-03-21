5d4753d94189bc9b71e76816aa364865bad09bf1
Jonanthan Bélanger <joe_belanger@hotmail.com>
2018-03-21 09:58:44 -0400

    Initialisation des Classes et Context

---
+++ TournoisGladiateur/Data/Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using TournoisGladiateur.Models;
+
+namespace TournoisGladiateur.Data
+{
+	public class Context : DbContext
+	{
+		public DbSet<Duel> Duels { get; set; }
+		public DbSet<Gladiateur> Gladiateurs { get; set; }
+		public DbSet<Round>	Rounds { get; set; }
+
+		public Context()
+		{
+			Database.SetInitializer(new DatabaseInitializer());
+		}
+
+	}
+}
---
+++ TournoisGladiateur/Data/DatabaseInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace TournoisGladiateur.Data
+{
+	internal class DatabaseInitializer : DropCreateDatabaseIfModelChanges<Context>
+	{
+		protected override void Seed(Context context)
+		{
+			base.Seed(context);
+		}
+	}
+}
